71fc3cc1345d866bac89aec09944e48bb67aba90
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-01-21 11:41:09 +0530

    Create 3set3.c

---
+++ 3set3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a[100],min,n,i;
+printf("enter the no of integers");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+min=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]<min)
+{
+min=a[i];
+}
+printf("%d",min);
+}
+return 0;
+}
+
+
