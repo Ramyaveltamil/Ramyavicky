83350068f6444f96ee2504c85b307f1ab639721f
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-01-25 16:12:52 +0530

    Create 3set8.c

---
+++ 3set8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[50],i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=o;i,n;i++);
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t %d",a[i],i);
+}
+return 0;
+}
