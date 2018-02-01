0bc3f68bbecbe802848f1b5c6067116c1ddd2fc8
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-02-01 11:05:58 +0000

    Create 3set10.c

---
+++ 3set10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+printf("Enter the first time");
+scanf("%d %d",&a1,&b1);
+printf("Enter the second time");
+scanf("%d %d",&a2,&b2);
+if(b2>b1);
+{
+b1+=60;
+--a1;
+}
+a3=a1-a2;
+b3=b1-b2;
+printf(" %d %d",a3,b3);
+return 0;
+}
