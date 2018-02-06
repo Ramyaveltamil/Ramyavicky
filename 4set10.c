1ed7f3fc9ae8355e732b87e7921b9da41edcb828
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-02-06 09:31:24 +0000

    Create 4set10.c

---
+++ 4set10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b ,c,i,n;
+printf("enter the limit:");
+scanf("%d",&n);
+a=0;
+b=1;
+c=0;
+for(i=0;i<n;i++)
+{
+printf("%d",c);
+a=b;
+b=c;
+c=a+b;
+}
+return 0;
+}
