da6014aa568c8b6776bf26d987c83205800eb987
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-02-13 08:28:48 +0000

    Create 6set3.c

---
+++ 6set3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,r,sum=0;
+printf("Enter a number");
+scanf("%d",&n);
+while(n>0)
+{
+r=n%10;
+sum=sum+r;
+n/=10;
+}
+printf("sum is %d",sum);
+return 0;
+}
+
