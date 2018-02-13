ab1de034ed269b32a8e7ab2b87faf8ba572ce802
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-02-13 09:21:52 +0000

    Update 6set6.c

--- 6set6.c
+++ 6set6.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 void main()
 {
-char s[100],i=0;
+char s[100];
+  int i=0;
 printf("enter a string");
 gets(s);
-while(s[i]!='/0')
+while(s[i]!='\0')
 {
 char ch=s[i];
-if((s[i]>='A' &&  s[i]<='Z') && (s[i]>='0' && s[i]>='9')) || (((s[i]>='a' &&  s[i]<='z') && (s[i]>='0' && s[i]>='9'))
+if((ch>='A' &&  ch<='Z') || (ch>='a' && ch<='z'))
 {
-printf(yes");
-i++;
-}
+  a++;
+  i++;
 }
 else
 printf("no");
