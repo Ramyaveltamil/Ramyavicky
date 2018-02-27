af54f6eb64a1a3c690c3205bf75383cbddcb4b9d
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-02-27 16:01:22 +0530

    Update 7set1.c

--- 7set1.c
+++ 7set1.c
@@ -3,16 +3,16 @@
 int main() 
 {
    char string[1000], sub[1000];
-   int position, length, c = 0;
+   int  length, c = 0;
  
    printf("Input a string\n");
    gets(string);
  
-   printf("Enter the position and length of substring\n");
-   scanf("%d%d", &position, &length);
+   printf("Enter the length of substring\n");
+   scanf("%d",  &length);
  
    while (c < length) {
-      sub[c] = string[position+c-1];
+      sub[c] = string[c];
       c++;
    }
    sub[c] = '\0';
