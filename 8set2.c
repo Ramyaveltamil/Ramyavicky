6a95cde66a28ced68cb08a0a0d3774f120f46394
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-03-01 16:30:08 +0530

    Create 8set2.c

---
+++ 8set2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+	char a[100];
+	int len,i,vow=0;
+	printf("\nENTER A STRING: ");
+	gets(a);
+	len=strlen(a);
+	for(i=0;i<len;i++)
+	{
+    if(a[i]=='a' || a[i]=='A' || a[i]=='e' || a[i]=='E' || a[i]=='i' || a[i]=='I' || a[i]=='o' || a[i]=='O' || a[i]=='u' || a[i]=='U')
+			vow=vow+1;
+	}
+
+     if(vow==0)
+     printf("no");
+     else
+     printf("yes");
+}
