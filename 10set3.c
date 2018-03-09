d2cabe4b014eb09f8b9681e3d41341e2796244cb
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-03-09 16:28:50 +0530

    Create 10set3.c

---
+++ 10set3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int c;
+    float k;
+   printf("enter the celsius:");
+   scanf("%d",&c);
+   k=c+273.15;
+   printf(" kelvin: %f",k);
+    return 0;
+} 
+    
+  
